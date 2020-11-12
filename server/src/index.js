a506bdd6dd334d639172c2ff5bfbbd58575bad1d
Pablo Maribondo <pablomgvieira@gmail.com>
2020-11-12 10:02:47 -0300

    Setup do mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const routes = require("./routes");
 
 const app = express();
+
+const uri = process.env.MONGO_ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
