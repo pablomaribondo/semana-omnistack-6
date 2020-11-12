680b75d1ed75ab7ff145843ce75aa7c117423d3a
Pablo Maribondo <pablomgvieira@gmail.com>
2020-11-12 15:07:49 -0300

    Rota de listagem de boxes implementada

--- server/src/controllers/BoxController.js
+++ server/src/controllers/BoxController.js
@@ -1,6 +1,19 @@
 const Box = require("../models/Box");
 
 class BoxController {
+  async show(request, response) {
+    const { id } = request.params;
+
+    const box = await Box.findById(id).populate({
+      path: "files",
+      options: {
+        sort: { createdAt: -1 },
+      },
+    });
+
+    return response.json(box);
+  }
+
   async store(request, response) {
     const { title } = request.body;
     const box = await Box.create({ title });
