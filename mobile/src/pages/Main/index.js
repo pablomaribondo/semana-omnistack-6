6997f4f0bd35223211e66d5c77f848469fa0c91a
Pablo Maribondo <pablomgvieira@gmail.com>
2021-01-11 16:11:49 -0300

    Tela de criação de box finalizada

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.png';
 import styles from './styles';
 
-const Main = () => {
+const Main = (props) => {
+  const {navigation} = props;
+  const [newBox, setNewBox] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const box = await AsyncStorage.getItem('@RocketBox:box');
+
+      if (box) {
+        navigation.navigate('Box');
+      }
+    })();
+  }, []);
+
+  const handleSignIn = async () => {
+    const response = await api.post('boxes', {
+      title: newBox,
+    });
+
+    await AsyncStorage.setItem('@RocketBox:box', response.data._id);
+
+    navigation.navigate('Box');
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={logo} />
@@ -16,9 +42,11 @@ const Main = () => {
         autoCapitalize="none"
         autoCorrect={false}
         underlineColorAndroid="transparent"
+        value={newBox}
+        onChangeText={(text) => setNewBox(text)}
       />
 
-      <TouchableOpacity onPress={() => {}} style={styles.button}>
+      <TouchableOpacity onPress={handleSignIn} style={styles.button}>
         <Text style={styles.buttonText}>Criar</Text>
       </TouchableOpacity>
     </View>
